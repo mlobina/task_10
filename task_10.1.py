77d0c543d28abbbb3f930a1816123d3d936fc9a5
Marina Lobina <mvlobina@gmsil.com>
2020-12-06 04:30:02 +0300

    task_10.1.py complete

--- task_10.1.py
+++ task_10.1.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+proxy = {'https': '207.180.238.252:8118'}
+url = 'https://superheroapi.com/api/2619421814940190/search/'
+hero_dict = {}
+heroes = ['Thanos', 'Hulk', 'Captain America', 'Batman']
+def get_hero_intelligence_dict(name):
+      response = requests.get(url + name, proxies=proxy)
+      hero_list = response.json()['results']
+
+      for hero in hero_list:
+          if name in hero.values():
+              hero_intelligence = hero['powerstats']['intelligence']
+              hero_dict.setdefault(name, int(hero_intelligence))
+      return hero_dict
+
+def main(heroes, max_number = 1):
+    for name in heroes:
+        dict = get_hero_intelligence_dict(name)
+
+    max_intelligence_dict = {}
+
+    max_intelligence_dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+    print(max_intelligence_dict)
+    for id, hero in enumerate(max_intelligence_dict):
+       if id == max_number:
+            break
+       print(f'The most intelligent superhero is {hero[0]} with intelligence {hero[1]}.')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main(heroes)
